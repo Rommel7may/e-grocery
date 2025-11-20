3340c43e226f595adad13383795d226dfc74bde8
Rommel7may <ml0997130@gmail.com>
2025-11-21 01:07:06 +0800

    Manage-users

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UsersController;
+use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
+
 
 Route::get('/', function () {
     return Inertia::render('welcome', [
@@ -18,5 +20,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
     Route::resource('products', ProductController::class);
 });
+
+Route::resource('manage-users', UsersController::class);
 
 require __DIR__.'/settings.php';
